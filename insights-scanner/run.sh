a760162cc6e01ad77c60e256b871115ed098024b
Brandon Williams <brandon.williams@amazee.io>
2026-02-09 08:14:44 -0600

    feat: update scanning script with latest from `build-deploy-tool`

--- insights-scanner/run.sh
+++ insights-scanner/run.sh
@@ -2,6 +2,59 @@
 
 DOCKER_HOST="${DOCKER_HOST:-docker-host.lagoon.svc}"
 
+# featureFlag searches for feature flag variables in the following locations
+# and order:
+#
+# 1. The cluster-force feature flag, prefixed with LAGOON_FEATURE_FLAG_FORCE_,
+#    as a scan pod environment variable. This is set via a flag on the
+#    build-deploy controller. This overrides the other variables and allows
+#    policy enforcement at the cluster level.
+#
+# 2. The regular feature flag, prefixed with LAGOON_FEATURE_FLAG_, in a
+#    Lagoon build scoped env-var. This allows policy control at the project
+#    level.
+#
+# 3. The cluster-default feature flag, prefixed with
+#    LAGOON_FEATURE_FLAG_DEFAULT_, as a scan pod environment variable. This is
+#    set via a flag on the build-deploy controller. This allows default policy
+#    to be set at the cluster level, but maintains the ability to selectively
+#    override at the project level.
+#
+# The value of the first variable found is printed to stdout. If the variable
+# is not found, print an empty string. Additional arguments are ignored.
+function featureFlag() {
+	# check for argument
+	[ "$1" ] || return
+
+	local forceFlagVar defaultFlagVar flagVar
+
+	# check build pod environment for the force policy first
+	forceFlagVar="LAGOON_FEATURE_FLAG_FORCE_$1"
+	[ "${!forceFlagVar}" ] && echo "${!forceFlagVar}" && return
+
+	flagValue=$(buildEnvVarCheck "LAGOON_FEATURE_FLAG_$1")
+	[ "$flagValue" ] && echo "$flagValue" && return
+
+	# fall back to the default, if set.
+	defaultFlagVar="LAGOON_FEATURE_FLAG_DEFAULT_$1"
+	echo "${!defaultFlagVar}"
+}
+
+# Checks for a build scoped env var from Lagoon API.
+function buildEnvVarCheck() {
+  # check for argument
+  [ "$1" ] || return
+
+  local flagVar
+
+  flagVar="$1"
+  # check Lagoon environment variables
+  flagValue=$(jq -r '.[] | select(.scope == "build" or .scope == "global") | select(.name == "'"$flagVar"'") | .value' <<< "$LAGOON_ENVIRONMENT_VARIABLES")
+  [ "$flagValue" ] && echo "$flagValue" && return
+
+  echo "$2"
+}
+
 # Read the comma-separated values
 IFS=','
 
